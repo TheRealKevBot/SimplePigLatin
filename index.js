e5e66597d0c7db9843eea2cf6a3d6303853d54f2
TheRealKevBot <Kglas001@gmail.com>
2021-08-13 17:20:08 -0600

    Build

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched. 
 
 function pigIt(str){
-    //Code here
+    arr = str.split(" ")
+    pig = []
+    for (var i = 0; i < arr.length; i++) { 
+        arr[i].match(/^[a-z]+$/i) ? pig.push(arr[i].substring(1) + arr[i][0] + "ay") : pig.push(arr[i])
+    }
+    return pig.join(' ')
 } 
 
 console.log(pigIt('Pig latin is cool'),'igPay atinlay siay oolcay')
