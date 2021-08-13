a91b29a163b75409efe566d99a1a933f20941ae2
TheRealKevBot <Kglas001@gmail.com>
2021-08-13 15:46:19 -0600

    First

---
+++ index.js
@@ -0,0 +1,8 @@
+// Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched. 
+
+function pigIt(str){
+    //Code here
+} 
+
+console.log(pigIt('Pig latin is cool'),'igPay atinlay siay oolcay')
+console.log(pigIt('This is my string'),'hisTay siay ymay tringsay')
